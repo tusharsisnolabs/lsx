120962f897a1b8444ce4409b0e02cdbef83b6ac4
Fernando Tessmann <nandotess85@gmail.com>
2016-09-28 13:31:47 -0300

    travis-ci.org

--- index.php
+++ index.php
@@ -21,32 +21,32 @@ get_header(); ?>
 
 		<main id="main" class="site-main">
 		
-		<?php lsx_content_top(); ?>
+			<?php lsx_content_top(); ?>
 
-		<?php if ( have_posts() ) : ?>
+			<?php if ( have_posts() ) : ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+					?>
 
-			<?php endwhile; ?>
-			
-			<?php lsx_paging_nav(); ?>
-			
-		<?php else : ?>
+				<?php endwhile; ?>
+				
+				<?php lsx_paging_nav(); ?>
+				
+			<?php else : ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
+				<?php get_template_part( 'content', 'none' ); ?>
 
-		<?php endif; ?>
+			<?php endif; ?>
 
-		<?php lsx_content_bottom(); ?>
+			<?php lsx_content_bottom(); ?>
 
 		</main><!-- #main -->
 
