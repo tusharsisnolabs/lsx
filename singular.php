319a47f4c7f534bf1074b6a7426bfb1d8c8f49df
Fernando Tessmann <nandotess85@gmail.com>
2017-05-17 17:13:44 -0300

    singular

--- singular.php
+++ singular.php
@@ -21,7 +21,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'partials/content', get_post_type() ); ?>
+				<?php get_template_part( 'partials/content', 'singular' ); ?>
 
 			<?php endwhile; ?>
 
