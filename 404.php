c8853e19b9b20f4aec38b2cf79e2d0099b502a30
Fernando Dalpiaz <fmdalpiaz@gmail.com>
2017-05-19 13:49:49 -0300

    Remove fixed message on the 404 page

--- 404.php
+++ 404.php
@@ -17,8 +17,6 @@ get_header(); ?>
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'lsx' ); ?></p>
 					<?php get_search_form(); ?>
-					<br>
-					<p><?php esc_html_e( 'Alternatively, you can check out the', 'lsx' ); ?> <a href="<?php echo esc_url( site_url( '/sitemap/' ) ); ?>"><strong><?php esc_html_e( 'sitemap', 'lsx' ); ?></strong></a></p>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
