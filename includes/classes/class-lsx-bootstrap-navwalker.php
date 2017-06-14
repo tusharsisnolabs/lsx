f825411c8c20d734ae958703e27f5f3dc09d0611
Fernando Tessmann <nandotess85@gmail.com>
2017-06-14 13:45:13 -0300

    removed reference to glyphicons

--- includes/classes/class-lsx-bootstrap-navwalker.php
+++ includes/classes/class-lsx-bootstrap-navwalker.php
@@ -137,19 +137,7 @@ if ( ! class_exists( 'LSX_Bootstrap_Navwalker' ) ) :
 
 				$item_output = $args->before;
 
-				/*
-				 * Glyphicons
-				 * ===========
-				 * Since the the menu item is NOT a Divider or Header we check the see
-				 * if there is a value in the attr_title property. If the attr_title
-				 * property is NOT null we apply it as the class name for the glyphicon.
-				 */
-				if ( ! empty( $item->attr_title ) ) {
-					$item_output .= '<a' . $attributes . '"><span class="glyphicon ' . esc_attr( $item->attr_title ) . '"></span>&nbsp;';
-				} else {
-					$item_output .= '<a' . $attributes . '>';
-				}
-
+				$item_output .= '<a' . $attributes . '>';
 				$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
 				$item_output .= ( $args->has_children && 0 === $depth ) ? ' <span class="caret"></span></a>' : '</a>';
 				$item_output .= $args->after;
