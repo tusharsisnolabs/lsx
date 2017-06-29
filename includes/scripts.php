90fd6baebe77efee9e2f14e9756f4548e624f102
Fernando Tessmann <nandotess85@gmail.com>
2017-06-29 11:22:58 -0300

    modernizer TEMP removed - let’s test if it’s still necessary

--- includes/scripts.php
+++ includes/scripts.php
@@ -116,15 +116,15 @@ if ( ! function_exists( 'lsx_scripts' ) ) :
 		/* @TODO - Check if is still necessary */
 		//wp_enqueue_script( 'mousewheel', get_template_directory_uri() . '/js/vendor/jquery.mousewheel.min.js', array( 'jquery' ), LSX_VERSION, true );
 		//wp_enqueue_script( 'smoothscroll', get_template_directory_uri() . '/js/vendor/jquery.simplr.smoothscroll.min.js', array( 'jquery' ), LSX_VERSION, true );
+		//wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-3.5.0.min.js', array(), LSX_VERSION, false );
 
 		wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/vendor/bootstrap.min.js', array( 'jquery' ), LSX_VERSION, true );
-		wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-3.5.0.min.js', array(), LSX_VERSION, false );
 
 		wp_enqueue_script( 'imagesLoaded', get_template_directory_uri() . '/assets/js/vendor/imagesloaded.pkgd.min.js', array( 'masonry' ), LSX_VERSION, true );
 		wp_enqueue_script( 'scrolltofixed', get_template_directory_uri() . '/assets/js/vendor/jquery-scrolltofixed-min.js', array( 'jquery' ), LSX_VERSION, true );
 		wp_enqueue_script( 'picturefill', get_template_directory_uri() . '/assets/js/vendor/picturefill.min.js', array(), LSX_VERSION, true );
 
-		wp_enqueue_script( 'lsx_script', get_template_directory_uri() . '/assets/js/lsx.min.js', array( 'jquery', 'bootstrap', 'modernizr', 'masonry', 'imagesLoaded', 'scrolltofixed', 'picturefill' ), LSX_VERSION, true );
+		wp_enqueue_script( 'lsx_script', get_template_directory_uri() . '/assets/js/lsx.min.js', array( 'jquery', 'bootstrap', 'masonry', 'imagesLoaded', 'scrolltofixed', 'picturefill' ), LSX_VERSION, true );
 
 		// Script parameters
 
